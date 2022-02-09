3976e67942be5680a98eb57e0c0b7031b4d8e1b6
Matheus unico <matheus8476@outlook.com>
2022-02-09 18:26:12 -0300

    refactor: tentativa de abertura de camera de docs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,13 +7,16 @@ import android.util.Log;
 import android.view.View;
 
 import com.acesso.acessobio_android.AcessoBioListener;
+import com.acesso.acessobio_android.iAcessoBioDocument;
 import com.acesso.acessobio_android.iAcessoBioSelfie;
 import com.acesso.acessobio_android.onboarding.AcessoBio;
 import com.acesso.acessobio_android.onboarding.IAcessoBioBuilder;
 import com.acesso.acessobio_android.onboarding.IAcessoBioTheme;
 import com.acesso.acessobio_android.onboarding.camera.UnicoCheckCamera;
 import com.acesso.acessobio_android.onboarding.camera.UnicoCheckCameraOpener;
+import com.acesso.acessobio_android.onboarding.camera.document.DocumentCameraListener;
 import com.acesso.acessobio_android.onboarding.camera.selfie.SelfieCameraListener;
+import com.acesso.acessobio_android.onboarding.types.DocumentType;
 import com.acesso.acessobio_android.services.dto.ErrorBio;
 import com.acesso.acessobio_android.services.dto.ResultCamera;
 
@@ -28,27 +31,56 @@ public class MainActivity extends AppCompatActivity implements AcessoBioListener
         this.acessoBioBuilder = new AcessoBio(this, this);
     }
 
+    iAcessoBioSelfie cameraListener = new iAcessoBioSelfie() {
+        @Override
+        public void onSuccessSelfie(ResultCamera result) {
+//            Log.d("Response", result.getEncrypted()); // getEncrypted() retorna o JWT
+            Log.d("Response", result.getBase64()); // getBase64() retorna o base64
+        }
+
+        @Override
+        public void onErrorSelfie(ErrorBio errorBio) {
+        }
+    };
+
+    iAcessoBioDocument cameraDocListener = new iAcessoBioDocument(){
+        @Override
+        public void onSuccessDocument(String base64) {
+            Log.d("Response", base64);
+        }
+
+        @Override
+        public void onErrorDocument(String error) {
+            System.out.println(error);
+        }
+    };
+
+
     public void openCamera(View view) {
-        UnicoCheckCamera unicoCheckCamera = this.acessoBioBuilder
-                .setAutoCapture(false)
-                .setSmartFrame(false)
-                .build();
+//        UnicoCheckCamera unicoCheckCamera = this.acessoBioBuilder
+//                .setAutoCapture(false)
+//                .setSmartFrame(false)
+//                .build();
+//
+//        unicoCheckCamera.prepareSelfieCamera("unico-check-mobile-services.json", new SelfieCameraListener() {
+//            @Override
+//            public void onCameraReady(UnicoCheckCameraOpener.Selfie cameraOpener) {
+//                cameraOpener.open(cameraListener);
+//            }
+//
+//            @Override
+//            public void onCameraFailed(String message) {
+//                System.out.println(message);
+//            }
+//        });
 
-        iAcessoBioSelfie cameraListener = new iAcessoBioSelfie() {
-            @Override
-            public void onSuccessSelfie(ResultCamera result) {
-                Log.d("Response", result.getEncrypted()); // getEncrypted() retorna o JWT
-                Log.d("Response", result.getBase64()); // getBase64() retorna o base64
-            }
 
-            @Override
-            public void onErrorSelfie(ErrorBio errorBio) { }
-        };
+        UnicoCheckCamera unicoCheckDocCamera = this.acessoBioBuilder.build();
 
-        unicoCheckCamera.prepareSelfieCamera("unico-check-mobile-services.json", new SelfieCameraListener() {
+        unicoCheckDocCamera.prepareDocumentCamera("unico-check-mobile-services.json", new DocumentCameraListener() {
             @Override
-            public void onCameraReady(UnicoCheckCameraOpener.Selfie cameraOpener) {
-                cameraOpener.open(cameraListener);
+            public void onCameraReady(UnicoCheckCameraOpener.Document cameraOpener) {
+                cameraOpener.open(DocumentType.CPF, cameraDocListener);
             }
 
             @Override
@@ -64,11 +96,14 @@ public class MainActivity extends AppCompatActivity implements AcessoBioListener
     }
 
     @Override
-    public void onUserClosedCameraManually() {}
+    public void onUserClosedCameraManually() {
+    }
 
     @Override
-    public void onSystemClosedCameraTimeoutSession() {}
+    public void onSystemClosedCameraTimeoutSession() {
+    }
 
     @Override
-    public void onSystemChangedTypeCameraTimeoutFaceInference() {}
+    public void onSystemChangedTypeCameraTimeoutFaceInference() {
+    }
 }
